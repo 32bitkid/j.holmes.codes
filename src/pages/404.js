b2fda8dc59792e4efb14b9dc65b2ea54d2f9983a
J Holmes <32bitkid@gmail.com>
2019-06-28 12:13:52 -0600

    quick style pass on 404

--- src/pages/404.js
+++ src/pages/404.js
@@ -3,11 +3,15 @@ import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
+import css from './404.module.scss';
+
 const NotFoundPage = () => (
   <Layout>
-    <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <SEO title="404" />
+    <main className={css.main}>
+      <h1>NOT FOUND</h1>
+      <p>You just hit a route that doesn&#39;t exist... oops.</p>
+    </main>
   </Layout>
 )
 
