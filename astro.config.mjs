9af27e7a435026bb54b71cc94fc23fad39e9183b
J Holmes <32bitkid@gmail.com>
2023-11-07 11:57:27 -0700

    more work porting things over

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,16 @@
+import mdx from "@astrojs/mdx";
 import { defineConfig } from 'astro/config';
 import remarkMath from "remark-math";
-
-import mdx from "@astrojs/mdx";
+import ViteYaml from '@modyfi/vite-plugin-yaml';
 import rehypeKatex from "rehype-katex";
+
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [mdx()],
+  vite: {
+    plugins: [ViteYaml()],
+  },
   markdown: {
     remarkPlugins: [remarkMath],
     rehypePlugins: [[rehypeKatex, {}]],
