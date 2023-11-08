5b51fac8edeee75d24ba0eb38cd159158106a10e
J Holmes <32bitkid@gmail.com>
2023-11-08 13:37:05 -0700

    adjust date formatting

--- src/components/parseDate.ts
+++ src/components/parseDate.ts
@@ -1,7 +1,8 @@
+import { parseISO } from "date-fns";
+
 export const parseDate = (input: unknown): Date => {
   if (input instanceof Date) return input;
-  if (typeof input === "string") return new Date(input);
+  if (typeof input === "string") return parseISO(input);
   if (typeof input === "number") return new Date(input);
-
   return new Date();
 }
