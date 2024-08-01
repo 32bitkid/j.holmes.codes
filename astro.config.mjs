df5893173d8a8b088a709d60bab07fa01b13239d
J Holmes <32bitkid@gmail.com>
2024-08-01 13:39:32 -0600

    New: Add support for definition lists in mdx files

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,11 +6,15 @@ import ViteYaml from '@modyfi/vite-plugin-yaml';
 import rehypeKatex from 'rehype-katex';
 import remarkHint from 'remark-hint';
 import remarkCapitalizeHeading from 'remark-capitalize-headings';
+import {
+  remarkDefinitionList,
+  defListHastHandlers,
+} from 'remark-definition-list';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://j.holmes.codes',
-  scopedStyleStrategy: "class",
+  scopedStyleStrategy: 'class',
   integrations: [
     mdx(),
     react({
@@ -25,7 +29,9 @@ export default defineConfig({
       remarkMath,
       remarkHint,
       [remarkCapitalizeHeading, { excludeHeadingLevel: { h1: true } }],
+      [remarkDefinitionList, {}],
     ],
+    remarkRehype: { handlers: { ...defListHastHandlers } },
     rehypePlugins: [[rehypeKatex, {}]],
     shikiConfig: {
       theme: 'monokai',
