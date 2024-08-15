c13bf59abd279883696a4fb2b4927286927210c7
J Holmes <32bitkid@gmail.com>
2024-08-14 18:14:27 -0600

    Featured: Adding support for featured conventional commits
    Inspired by a random thought I had on mastodon: this site now generates a `/changelog` page that is generated from the `git` commit history. Aside from general statistics. it will grab the *contents* of commits like this one and render them as *Featured Changesets* during compilation. I don't know if I've ever seen anybody else talk about doing something _quite_ like this, so I think this could be pretty *interesting* to explore in the future.
    
    I'm a big fan of `git blame` and other VCS-centric annotation systems. I have always *tried* to curate *meaningful* commit messages as the "ideal". So, being able to embed a microblog entry in the commit history, but still have them accessible on the web feels really nice. I also think that it might, at least for me, lower the barrier to entry to *write about* the changes that I make to the site, that maybe I normally *wouldn't*. Because while I don't *have* to write a blog about my work, I *always* make a commit.
    
    So, we'll see where this goes… but I'm _excited_.

---
+++ src/utils/git.ts
@@ -0,0 +1,51 @@
+import { GitClient } from '@conventional-changelog/git-client';
+import type { GitLogParams, Params } from '@conventional-changelog/git-client';
+
+interface CommitFormatter<T> {
+  format: string;
+  parseFn: (raw: string) => T;
+}
+
+async function getCommits<T>(
+  client: GitClient,
+  { format, parseFn }: CommitFormatter<T>,
+  params?: GitLogParams & Params,
+): Promise<T[]> {
+  const results: T[] = [];
+  for await (const row of client.getRawCommits({ ...params, format }))
+    results.push(parseFn(row));
+  return results;
+}
+
+const SUMMARY: CommitFormatter<string> = {
+  format: '%s',
+  parseFn: (it) => it,
+};
+
+export async function getCommitSummary(
+  client: GitClient,
+  params?: GitLogParams & Params,
+): Promise<string[]> {
+  return getCommits<string>(client, SUMMARY, params);
+}
+
+type Commit = { hash: string; date: Date; subject: string; body: string };
+const DETAILS: CommitFormatter<Commit> = {
+  format: '%h\x1f%cI\x1f%s\x1f%b',
+  parseFn(msg) {
+    const [hash, date, subject, body] = msg.split('\x1f');
+    return {
+      hash,
+      date: new Date(date),
+      subject: subject.trim(),
+      body: body.trim(),
+    };
+  },
+};
+
+export async function getCommitDetails(
+  client: GitClient,
+  params?: GitLogParams & Params,
+): Promise<Commit[]> {
+  return getCommits(client, DETAILS, params);
+}
